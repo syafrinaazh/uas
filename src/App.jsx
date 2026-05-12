80abfb95190b989cf28cd2e48bc058d84fa9a93d
Syafrina <syafrinazhr11@gmail.com>
2026-05-12 08:50:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Beranda from './components/Beranda'
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex justify-center items-center">
+      {/* Container ini dibuat menyerupai tampilan HP (max-w-md) */}
+      <div className="w-full max-w-md h-screen overflow-y-auto bg-white shadow-2xl">
+        <Beranda />
+      </div>
+    </div>
+  )
+}
+
+export default App
